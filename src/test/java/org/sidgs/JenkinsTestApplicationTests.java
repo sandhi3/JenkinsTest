dc078a3c085abaf254e850fd5aa09e11929adc8d
Sandhi  Raghuvaran / SID Global <sandhir@sidgs.com>
2023-04-19 15:09:07 +0530

    first commit

---
+++ src/test/java/org/sidgs/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sidgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
